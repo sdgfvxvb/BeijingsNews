02eae090db49273e0266bbfa2075005a84c8eb2b
396220003@qq.com <396220003@qq.com>
2020-12-16 20:53:49 +0800

    添加了slidingMenu的左侧菜单

--- app/src/main/java/com/example/beijingnews/activity/MainActivity.java
+++ app/src/main/java/com/example/beijingnews/activity/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.beijingnews.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.beijingnews.R;
+import com.example.beijingnews.utils.DensityUtils;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        设置主页面
         setContentView(R.layout.activity_main);
+
+//        设置左侧菜单
+        setBehindContentView(R.layout.activity_leftmenu);
+
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setBehindOffset(DensityUtils.dip2px(this, 200));
     }
 }
