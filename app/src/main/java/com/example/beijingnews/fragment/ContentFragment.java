a091d17cfda972d38ed208d6a21beec331946684
396220003@qq.com <396220003@qq.com>
2020-12-18 23:21:43 +0800

    增加底部菜单

--- app/src/main/java/com/example/beijingnews/fragment/ContentFragment.java
+++ app/src/main/java/com/example/beijingnews/fragment/ContentFragment.java
@@ -3,27 +3,31 @@ package com.example.beijingnews.fragment;
 import android.graphics.Color;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.beijingnews.R;
 import com.example.beijingnews.base.BaseFragment;
 
 public class ContentFragment extends BaseFragment {
 
-    private TextView textView;
+    private ViewPager viewPager;
+    private RadioGroup rg_main;
 
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(23);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(context, R.layout.content_fragment, null);
+        viewPager = view.findViewById(R.id.view_page);
+        rg_main = view.findViewById(R.id.rg_main);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("主页面");
+        rg_main.check(R.id.rb_home);
     }
 
 }
