c32ea5560eca29bcd0e64d475662350a1937661e
396220003@qq.com <396220003@qq.com>
2020-12-23 21:38:31 +0800

    修改左侧菜单的实现方式。

--- app/src/main/java/com/example/beijingnews/activity/MainActivity.java
+++ app/src/main/java/com/example/beijingnews/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.beijingnews.activity;
 
 import android.app.AppComponentFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
